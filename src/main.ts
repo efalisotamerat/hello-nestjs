d34269ce4662e4e9e8b1d37ddb4faef7690654ac
Efaliso Tamerat <efalisotamerat@gmail.com>
2024-07-18 15:30:32 +0200

    techdocs

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const openApiConfig = new DocumentBuilder()
     .setTitle('Users API')
     .setDescription('All public endpoints for the Users API')
